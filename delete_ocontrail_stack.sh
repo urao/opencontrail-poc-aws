6f95fc977c79cb1ddd8fe04717bb0194c8c09724
urao <urao@juniper.net>
2017-08-06 23:07:31 -0700

    more changes

--- delete_ocontrail_stack.sh
+++ delete_ocontrail_stack.sh
@@ -2,15 +2,20 @@
 
 stackname=$1
 
-if [ -z "$stackname" ]; then
-    echo "stack name not set"
-    exit 1
+#check if there are 3 arguments
+if [ $# -eq 1 ]; then
+   if [ -z "$stackname" ]; then
+       echo "stack name not set"
+       exit 1
+   fi
+else
+   echo "Usage: $0 [stackname]" 
+   exit 1
 fi
 
-echo "Deleting opencontrail $stackname stack for POC"
+echo -e "Deleting opencontrail $stackname stack for POC\n"
 
 aws cloudformation delete-stack --stack-name $stackname 
 
-
-echo "Wait till opencontrail stack($stackname) is deleted\n"
+echo -e "Wait till opencontrail stack($stackname) is deleted\n"
 aws cloudformation wait stack-delete-complete --stack-name $stackname && aws cloudformation describe-stacks --stack-name $stackname
--- verify_ocontrail_stack.sh
+++ verify_ocontrail_stack.sh
@@ -2,7 +2,7 @@
 
 stackname=$1
 
-#check if there are 3 arguments
+#check if there are enough arguments
 if [ $# -eq 1 ]; then
    if [ -z "$stackname" ]; then
        echo "stack name not set"
@@ -21,39 +21,71 @@ echo -e "Checking wether EC2 instances are UP and RUNNING\n"
 ctrlname=$(cat cloudformation/contrail/cstack-parameters.json | grep CCName1 -A2 | awk '{print $2}' | sed -n 2p)
 computename=$(cat cloudformation/contrail/cstack-parameters.json | grep CCName2 -A2 | awk '{print $2}' | sed -n 2p)
 
-echo $ctrlname
-echo $computename
+#echo $ctrlname
+#echo $computename
 
 aws ec2 wait instance-running --filters "Name=tag:Name,Values="$ctrlname""
 aws ec2 wait instance-running --filters "Name=tag:Name,Values="$computename""
 
-echo "EC2 insances are RUNNING"
+echo -e "EC2 insances are RUNNING\n"
 echo
 
-echo "[contrailc]">>$DIR/ansible/playbook/inventory/hosts
+# delete hosts file
+rm -rf $DIR/ansible/playbook/inventory/hosts
+
+echo "[controller]">>$DIR/ansible/playbook/inventory/hosts
 contrailc_ip=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$ctrlname" | grep PublicIpAddress | cut -d':' -f2 | tr -d '", ')
 echo "$contrailc_ip">>$DIR/ansible/playbook/inventory/hosts
 echo "">>$DIR/ansible/playbook/inventory/hosts
 
-echo "[compute01]">>$DIR/ansible/playbook/inventory/hosts
+echo "[compute]">>$DIR/ansible/playbook/inventory/hosts
 compute01_ip=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$computename" | grep PublicIpAddress | cut -d':' -f2 | tr -d '", ')
 echo "$compute01_ip">>$DIR/ansible/playbook/inventory/hosts
 echo "">>$DIR/ansible/playbook/inventory/hosts
 
-echo "ContrailC and Compute01 IP.................."
-echo ContrailC: "${contrailc_ip}"
-echo Compute01: "${compute01_ip}"
+#echo "ContrailC and Compute01 IP.................."
+#echo ContrailC: "${contrailc_ip}"
+#echo Compute01: "${compute01_ip}"
 
 # Update testbed.py file with private address
 contrailc_pip=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$ctrlname" | grep "\<PrivateIpAddress\>" | cut -d':' -f2 | tr -d '", ' | sort -u)
 
 compute01_pip=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$computename" | grep "\<PrivateIpAddress\>" | cut -d':' -f2 | tr -d '", ' | sort -u)
 
-echo "ctrlname=$ctrlname">>$DIR/ansible/playbook/inventory/group_vars/all.yml
-echo "computename=$computename">>$DIR/ansible/playbook/inventory/group_vars/all.yml
+# delete all.yml file
+rm -rf $DIR/ansible/playbook/inventory/group_vars/all.yml
 
-echo ContrailCp: "${contrailc_pip}"
-echo Compute01p: "${compute01_pip}"
+# create new one
+cat > $DIR/ansible/playbook/inventory/group_vars/all.yml <<EOF
+###################################################
+# Ansible specific vars
+##
 
-sed -i "s/1.1.1.1/$contrailc_pip/" $DIR/ansible/files/testbed_single.py
-sed -i "s/1.1.1.2/$compute01_pip/" $DIR/ansible/files/testbed_single.py
+# ansible connection details
+ansible_user: root
+ansible_connection: ssh
+ansible_ssh_pass: c0ntrail123
+host_key_checking: false
+
+###################################################
+# Common settings for contrail
+##
+
+# contrail package
+contrail_package:
+
+EOF
+
+echo "controllername: $(sed -e 's/^"//' -e 's/"$//' <<<"$ctrlname")">>$DIR/ansible/playbook/inventory/group_vars/all.yml
+echo "computename: $(sed -e 's/^"//' -e 's/"$//' <<<"$computename")">>$DIR/ansible/playbook/inventory/group_vars/all.yml
+
+echo "">>$DIR/ansible/playbook/inventory/group_vars/all.yml
+echo "">>$DIR/ansible/playbook/inventory/group_vars/all.yml
+
+echo "controller_ip: $contrailc_pip">>$DIR/ansible/playbook/inventory/group_vars/all.yml
+echo "compute_ip: $compute01_pip">>$DIR/ansible/playbook/inventory/group_vars/all.yml
+echo "">>$DIR/ansible/playbook/inventory/group_vars/all.yml
+echo "">>$DIR/ansible/playbook/inventory/group_vars/all.yml
+#echo ContrailCp: "${contrailc_pip}"
+#echo Compute01p: "${compute01_pip}"
+
